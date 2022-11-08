9ab6dd247f407a3ec36c8dfdfa59264f059cc869
ckackarov <107988223+ckackarov@users.noreply.github.com>
2022-11-08 19:04:56 +0200

    Update SearchComponent.jsx

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -8,8 +8,7 @@ const SearchComponent = () => {
   };
 
   return (
-    <div className="mt-[3rem]">
-     
+    <div className="mt-[3rem]"
         <input
           className="w-[250px] py-[.5rem] px-[1rem] outline-none border rounded-tl-md rounded-bl-md text-slate-700 bg-slate-400 font-medium placeholder:text-sky-50"
           type="text"
@@ -19,8 +18,7 @@ const SearchComponent = () => {
         ></input>
         <button className="px-[1rem] py-[.5rem] border rounded-tr-md rounded-br-md hover:bg-slate-700 font-medium active:scale-[.98] transition-all ">
           Search
-        </button>
-      
+        </button>      
     </div>
   );
 };
