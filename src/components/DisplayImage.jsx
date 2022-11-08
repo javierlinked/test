a3bae6150a7fd7fc88fd5e454f79ce29090c6670
ckackarov <107988223+ckackarov@users.noreply.github.com>
2022-11-08 19:06:11 +0200

    Update DisplayImage.jsx

--- src/components/DisplayImage.jsx
+++ src/components/DisplayImage.jsx
@@ -4,7 +4,7 @@ const DisplayImage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <img
-        className="w-[800px]   object-cover opacity-20"
+        className="w-[800px]   object-cover"
         src="https://images.unsplash.com/photo-1667892063532-2a39a634acef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1750&q=80"
       ></img>
     </div>
