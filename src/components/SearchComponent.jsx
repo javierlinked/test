d46dbd70ac71a0fa3de21fce684146cbea9c3c28
ckackarov <107988223+ckackarov@users.noreply.github.com>
2022-11-08 19:21:00 +0200

    Update SearchComponent.jsx

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -7,8 +7,13 @@ const SearchComponent = () => {
     setSearchValue(e.target.value);
   };
 
+  const handleButtonSearch = () => {
+    setSearchValue("")
+  }
+
   return (
-    <div className="mt-[3rem]"
+    <div className="mt-[3rem]">
+      <form>
         <input
           className="w-[250px] py-[.5rem] px-[1rem] outline-none border rounded-tl-md rounded-bl-md text-slate-700 bg-slate-400 font-medium placeholder:text-sky-50"
           type="text"
@@ -16,9 +21,12 @@ const SearchComponent = () => {
           value={searchValue}
           onChange={handleInputChange}
         ></input>
-        <button className="px-[1rem] py-[.5rem] border rounded-tr-md rounded-br-md hover:bg-slate-700 font-medium active:scale-[.98] transition-all ">
+        <button 
+        onclick={handleButtonSearch}
+        className="px-[1rem] py-[.5rem] border rounded-tr-md rounded-br-md hover:bg-slate-700 font-medium active:scale-[.98] transition-all ">
           Search
-        </button>      
+        </button>
+      </form>
     </div>
   );
 };
